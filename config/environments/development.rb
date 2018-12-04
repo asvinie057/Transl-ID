fbf4c95b38e07994c24edd5e53cb9c2cb9f03fbd
Rumkox <camilo42.es@gmail.com>
2018-12-04 11:21:10 +0100

    [developement] v: App.h.erb - 1 ligne

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -60,4 +60,48 @@ Rails.application.configure do
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
+  Ruby
+
+  config.action_mailer.default_url_options = { host: '137.74.175.51', port: 3001 }
+  
+  config.action_mailer.perform_caching = false 
+  
+  config.action_mailer.perform_deliveries = true
+  
+  config.action_mailer.raise_delivery_errors = true
+  
+  config.action_mailer.delivery_method = :smtp
+  
+  config.action_mailer.smtp_settings = {
+    user_name:      Rails.application.secrets.mail_username,
+    password:       Rails.application.secrets.mail_password,
+    domain:         'gmail.com',
+    address:       'smtp.gmail.com',
+    port:          '587',
+    authentication: :plain,
+    enable_starttls_auto: true
+  }
+
+
+  config.action_mailer.default_url_options = { host: '137.74.175.51', port: 3001 }
+  
+  config.action_mailer.perform_caching = false 
+  
+  config.action_mailer.perform_deliveries = true
+  
+  config.action_mailer.raise_delivery_errors = true
+  
+  config.action_mailer.delivery_method = :smtp
+  
+  config.action_mailer.smtp_settings = {
+    user_name:      Rails.application.secrets.mail_username,
+    password:       Rails.application.secrets.mail_password,
+    domain:         'gmail.com',
+    address:       'smtp.gmail.com',
+    port:          '587',
+    authentication: :plain,
+    enable_starttls_auto: true
+  }
+  
 end
