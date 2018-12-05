ef891200209ae2ee927462170669e094f6afde5c
Rumkox <camilo42.es@gmail.com>
2018-12-05 15:51:44 +0100

    [devise] validates

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ApplicationRecord
+
+  validates :first_name, presence: true, format: { with: /\A[a-zA-Z]*\z/ }
+  validates :last_name, presence: true, format: { with: /\A[a-zA-Z]*\z/ }
+  validates :phone_number, presence: true, format: { with: /\A[0-9+]*\z/ }, length: { minimum: 8, maximum: 13 }
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  # :trackable, :confirmable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, :trackable
 end
